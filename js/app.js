bf85cb832ffafe9052470e9ed01250171664511e
Supakorn <supakorncharoen@gmail.com>
2021-11-23 16:57:29 +0700

    add Barba.js which help you added the page-transition

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+let indexEnter = () => {
 let tl = gsap.timeline()
 tl.fromTo('.logo',{
     x: -200,
@@ -116,4 +117,175 @@ tl.fromTo('.logo',{
     y: 0,
     opacity: 1,
     duration: .6
-}, );
+}, )
+};
+
+let delay = (n) => {
+    return new Promise((done) => {
+        setTimeout(() => {
+            done();
+        }, n)
+    })
+};
+let loadingLeave = () => {
+    let tl = gsap.timeline();
+    tl.fromTo(".loading-bg", {
+        y:'100%'
+    },{
+        y: 0,
+        duration: 2
+    })
+};
+
+let loadingEnter = () => {
+    let tl = gsap.timeline();
+    tl.fromTo(".loading-bg", {
+        y:0
+    },{
+        y: '100%',
+        duration: 2
+    })
+};
+
+let galleryEnter = () => {
+    let tl = gsap.timeline();
+    tl.fromTo('.logo',{
+        x: -200,
+        opacity: 0,
+    },{
+        x: 0,
+        opacity: 1,
+        delay: .5,
+        duration: 1.3
+    })
+    .fromTo('.menu',{
+        x: 200,
+        opacity: 0,
+    },{
+        x: 0,
+        opacity: 1,
+        delay: .5,
+        duration: 1.3
+    },
+    ">-2")
+    .fromTo('.white-bg ',
+    {
+        y: 50,
+        opacity: 0
+    },{
+        y: 0,
+        opacity: 1,
+        duration: .8,
+        ease: 'power1.inOut'
+    })
+    .fromTo('.white-bg ul li',
+    {
+        y: 50,
+        opacity: 0
+    },{
+        y: 0,
+        opacity: 1,
+        duration: .2,
+        stagger: .1,
+        ease: 'power1.inOut'
+    });
+}
+
+let aboutEnter = () => {
+    let tl = gsap.timeline()
+    tl.fromTo('.logo',{
+        x: -200,
+        opacity: 0,
+    },{
+        x: 0,
+        opacity: 1,
+        delay: .5,
+        duration: 1.3
+    })
+    .fromTo('.menu',{
+        x: 200,
+        opacity: 0,
+    },{
+        x: 0,
+        opacity: 1,
+        delay: .5,
+        duration: 1.3
+    },
+    ">-1.8")
+    .fromTo('.info-section h1',{
+        x: 0,
+        y: 100,
+        opacity: 0,
+    },{
+        x: 0,
+        y: 0,
+        opacity: 1,
+        duration: 1.3
+    })
+}
+
+
+barba.init({
+    sync: true,
+    transitions: [
+        {
+            name: 'page-wipe',
+            async leave(data){
+                let done = this.async();
+                console.log('Leaving Page Animation');
+                loadingLeave();
+                await delay(1500);
+                done();
+            },
+            async enter(data){
+                loadingEnter();
+                console.log('Entering Page Animation');
+            }
+        },
+        {
+            name: 'gallery-transition',
+            from: {
+                namespace: ['home', 'about']
+            }, 
+            to: {
+                namespace: ['gallery']
+            },
+            async leave(data){
+                let done = this.async();
+                console.log('Leaving Page Animation');
+                loadingLeave();
+                await delay(1500);
+                done();
+            },
+            async enter(data){
+                loadingEnter();
+                galleryEnter();
+                console.log('Entering Page Animation');
+            }
+        }
+    ],
+    views: [{
+        namespace: 'gallery',
+        afterEnter(data) {
+          loadingEnter();
+          galleryEnter();
+        }
+      },
+      {
+        namespace: 'index',
+        afterEnter(data) {
+          loadingEnter();
+          indexEnter();
+        }
+      },
+      {
+        namespace: 'about',
+        afterEnter(data) {
+        loadingEnter();
+        aboutEnter();
+        }
+      }]
+});
+
+
+
